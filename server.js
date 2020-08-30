1172eb52427462f0adad9e18376663699c9b644b
Rohit Ambre <rohit003.ambre@gmail.com>
2020-08-30 21:08:26 +0530

    feat: :zap: initial commit with basic fastify server

---
+++ server.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+fastify.post('/', async (request, reply) => {
+  console.log(request.body)
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
