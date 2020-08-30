3278033b368a65a4f039c95030ee51bf34facf5c
Rohit Ambre <rohit003.ambre@gmail.com>
2020-08-31 00:21:36 +0530

    chore: :pencil: used dotnev

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const fastify = require('fastify')({ logger: true });
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3003;
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+  return { hello: 'world' };
+});
 
 fastify.post('/', async (request, reply) => {
-  console.log(request.body)
-  return { hello: 'world' }
-})
+  return { hello: 'world' };
+});
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(PORT)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    await fastify.listen(PORT);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
 }
-start()
+start();
