4ea7db40c87bbba75e5c6550c4feea98b05db634
Rohit Ambre <rohit003.ambre@gmail.com>
2020-09-15 00:40:14 +0530

    feat: :zap: connected Mongo using mongoose

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const apiRoutes = require('./src/routes');
 
 const PORT = process.env.PORT || 3003;
-
+const MONGO_CONN = process.env.MONGO_URI;
 fastify.register(apiRoutes, { prefix: '/api' });
+
+// Mongo connection
+mongoose
+  .connect(MONGO_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    fastify.log.info('Mongo connected succesfully');
+  })
+  .catch((err) => {
+    fastify.log.error(`Error in mongo connection: ${err}`);
+  });
 
 // Run the server!
 const start = async () => {
