4674ef029afe6f73726c34a674899b727fdc902b
Rohit Ambre <rohit003.ambre@gmail.com>
2020-09-16 01:28:20 +0530

    feat: :sparkles: WIP: created basic user create api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,14 @@
+const User = require('../models/user.model');
+
 module.exports = function (fastify, opts, done) {
-  fastify.get('/', (_, rpl) => {
-    rpl.code(200).send('all ok here');
+  fastify.post('/signup', (req, rpl) => {
+    const user = new User(req.body);
+    user.save((err, newUser) => {
+      if (err) {
+        fastify.log.error(err);
+      }
+      rpl.code(200).send(newUser);
+    });
   });
   done();
 };
