51c74358405b2a4f3f6a242edba3d7ebc91f8faa
Rohit Ambre <rohit003.ambre@gmail.com>
2020-10-16 00:38:28 +0530

    feat: :zap: added get all users controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,29 @@
+const User = require('../models/user.model');
+
 const getAllUsers = (req, rpl) => {
-  rpl.send('getAllUsers');
+  User.find(
+    {},
+    '_id email firstName lastName createdAt updatedAt',
+    (err, users) => {
+      if (err) {
+        console.log(`DB Error: ${err.message}`);
+        rpl.status(500).send({
+          status: false,
+          message: 'some error occured',
+          error: err
+        });
+      }
+      if (users) {
+        rpl
+          .status(200)
+          .send({ status: true, data: users, message: 'List of all users' });
+      } else {
+        rpl
+          .status(200)
+          .send({ status: false, data: null, message: 'No users found' });
+      }
+    }
+  );
 };
 
 module.exports = {
